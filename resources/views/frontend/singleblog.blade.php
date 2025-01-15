d3cb13ccc26bb9009c331115ccac7d766530aed0
Vannak Dim <vannakhannah@gmail.com>
2025-01-15 22:06:28 +0700

    home page re-design

--- resources/views/frontend/singleblog.blade.php
+++ resources/views/frontend/singleblog.blade.php
@@ -33,14 +33,8 @@
                     <article class="entry entry-single" data-aos="fade-up">
 
                         <div class="entry-img">
-                            <img id="postImage" src="{{ asset($post->image) }}" alt="" class="img-fluid">
-                        </div>
-
-                        <!-- The Modal -->
-                        <div id="imageModal" class="modal">
-                            <span class="close">&times;</span>
-                            <img class="modal-content" id="fullImage">
-                            <div id="caption"></div>
+                            <img src="{{ asset($post->image) }}" alt="" class="img-fluid image-container"
+                                onclick="openFullscreen(this)">
                         </div>
 
                         <h2 class="entry-title">
@@ -68,8 +62,8 @@
                                 <i class="icofont-folder"></i>
                                 <ul class="cats">
                                     @foreach ($post->categories as $category)
-                                        <li><a
-                                                href="{{ route('filter_by_category', $category->name) }}">{{ $category->name }}</a>
+                                        <li>
+                                            <a href="{{ route('filter_by_category', $category->name) }}">{{ $category->name }}</a>
                                         </li>
                                     @endforeach
                                 </ul>
@@ -268,100 +262,52 @@
             font-family: 'battambang';
         }
 
-        /* The Modal (background) */
-        .modal {
+        /* styles.css */
+        .fullscreen-image {
             display: none;
+            /* Hidden by default */
             position: fixed;
-            z-index: 1;
-            padding-top: 100px;
-            left: 0;
             top: 0;
+            left: 0;
             width: 100%;
             height: 100%;
-            overflow: auto;
-            background-color: rgb(0, 0, 0);
             background-color: rgba(0, 0, 0, 0.9);
+            z-index: 1000;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
         }
 
-        /* Modal Content (image) */
-        .modal-content {
+        .fullscreen-image img {
+            max-width: 90%;
+            max-height: 90%;
             margin: auto;
-            display: block;
-            width: 80%;
-            max-width: 700px;
-        }
-
-        /* Caption of Modal Image */
-        #caption {
-            margin: auto;
-            display: block;
-            width: 80%;
-            max-width: 700px;
-            text-align: center;
-            color: #ccc;
-            padding: 10px 0;
-        }
-
-        /* Add Animation */
-        .modal-content,
-        #caption {
-            -webkit-animation-name: zoom;
-            -webkit-animation-duration: 0.6s;
-            animation-name: zoom;
-            animation-duration: 0.6s;
-        }
-
-        @-webkit-keyframes zoom {
-            from {
-                -webkit-transform: scale(0)
-            }
-
-            to {
-                -webkit-transform: scale(1)
-            }
-        }
-
-        @keyframes zoom {
-            from {
-                transform: scale(0)
-            }
-
-            to {
-                transform: scale(1)
-            }
-        }
-
-        /* The Close Button */
-        .close {
-            position: absolute;
-            top: 15px;
-            right: 35px;
-            color: #f1f1f1;
-            font-size: 40px;
-            font-weight: bold;
-            transition: 0.3s;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: #bbb;
-            text-decoration: none;
-            cursor: pointer;
         }
     </style>
 
     <script>
-        document.getElementById('postImage').onclick = function() {
-            var modal = document.getElementById('imageModal');
-            var modalImg = document.getElementById('fullImage');
-            var captionText = document.getElementById('caption');
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
+        // script.js
+        function openFullscreen(image) {
+            // Create a div for the fullscreen overlay
+            let fullscreenDiv = document.createElement('div');
+            fullscreenDiv.classList.add('fullscreen-image');
 
-            var span = document.getElementsByClassName('close')[0];
-            span.onclick = function() {
-                modal.style.display = "none";
+            // Create an img element for the fullscreen image
+            let fullscreenImg = document.createElement('img');
+            fullscreenImg.src = image.src;
+
+            // Append the image to the fullscreen div
+            fullscreenDiv.appendChild(fullscreenImg);
+
+            // Append the fullscreen div to the body
+            document.body.appendChild(fullscreenDiv);
+
+            // Show the fullscreen image
+            fullscreenDiv.style.display = 'flex';
+
+            // Close the fullscreen on click
+            fullscreenDiv.onclick = function() {
+                fullscreenDiv.remove();
             }
         }
     </script>
